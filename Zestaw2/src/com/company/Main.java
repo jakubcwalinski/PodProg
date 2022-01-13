81dead11e16c09d38846cc8a215dc10c6bf9173c
jakubcwalinski <jakub.cwalinski2@gmail.com>
2022-01-13 23:33:12 +0100

    testifitworksv2

--- Zestaw2/src/com/company/Main.java
+++ Zestaw2/src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+
     public static void main(String[] args) {
-	// write your code here
+
+
+        int[] tab = new int[8];
+
+        for (int j = tab.length-1; j >=0; j++) {
+            Scanner scanner = new Scanner(System.in);
+            i = scanner.nextInt();
+            i++;
+            System.out.println(i);
+
+            if (i == i+1){
+                System.out.println("dupa");
+            }
+        }
     }
 }
